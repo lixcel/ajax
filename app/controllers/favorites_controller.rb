3d2590a376db51d817e861284aeda509fd1b2f13
EC2 Default User <ec2-user@ip-172-31-88-232.ec2.internal>
2022-02-01 05:40:42 +0000

    [add]いいね機能非同期通信化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,23 @@
 class FavoritesController < ApplicationController
 
+  before_action :set_variable
+
+
   def create
-    @book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
   end
 
   def destroy
-    @book = Book.find(params[:book_id])
+
     favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
+  end
+
+  private
+
+  def set_variable
+    @book = Book.find(params[:book_id])
   end
 
 end
