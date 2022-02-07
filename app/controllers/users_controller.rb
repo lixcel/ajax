bfff9bda1923f248282168f5575320f82b3622aa
EC2 Default User <ec2-user@ip-172-31-88-232.ec2.internal>
2022-02-07 10:58:51 +0000

    [Finis]9b途中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,19 @@ class UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:update, :edit]
   before_action :authenticate_user!, only: [:following, :followers]
 
+
+  def search
+    @user = User.find(params[:user_id])
+    @books = @user.books
+    @book = Book.new
+    if params[:created_at] == ""
+      @search_book = "日付を選択してください。"
+    else
+      create_at = params[:created_at]
+      @search_book = @books.where(['created_at LIKE ? ', "#{create_at}%"]).count
+    end
+  end
+
   def show
     @user = User.find(params[:id])
     @book = Book.new
